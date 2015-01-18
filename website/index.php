f00dd64f9cdec6d521031c175bca7dffbd82353c
marcusmeissner <marcusmeissner@67ed7778-7388-44ab-90cf-0a291f65f57c>
2015-01-18 21:12:31 +0000

    2.5.7 released
    git-svn-id: svn://svn.code.sf.net/p/gphoto/code/trunk@15337 67ed7778-7388-44ab-90cf-0a291f65f57c

--- website/index.php
+++ website/index.php
@@ -17,6 +17,14 @@
 
 
     	<p>
+	<b>libgphoto2 2.5.7</b> has been
+	released. Read <a href="/news/" alt="News"><b>what's new</b></a>.
+	Download it <a href="/download/" alt="Download"><b>now</b></a>.
+    	</p>
+	<div style="text-align: right;"><b>January 18th, 2015</b></div>
+    	<p>
+
+    	<p>
 	<b>libgphoto2 and gphoto2 2.5.6</b> have been
 	released. Read <a href="/news/" alt="News"><b>what's new</b></a>.
 	Download it <a href="/download/" alt="Download"><b>now</b></a>.
